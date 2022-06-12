2f30034df53a2edc63d109c494a84f453fc0f35d
Ramyuser <89673479+Ramyuser@users.noreply.github.com>
2022-06-11 19:19:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+song = "";
+
+function preload() {
+    song = loadSound("music2.mp3");
+    song = loadSound("music.mp3");
+}
